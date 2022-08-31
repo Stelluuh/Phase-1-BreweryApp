91e19049abe09c5aa51d25bf385c3f8c0fe46c67
stellamunson <sftamesis13@gmail.com>
2022-08-31 10:05:20 -0700

    changed element to button on lines 24 and 28

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /*------------------CREATE VARIABLES------------------*/
 // alert('It works!')
 let allBreweries = [] 
@@ -20,11 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
    fetchandCopyData();
 })
 
-allButtons.forEach(element => element.addEventListener('mouseover', (e) => {
+allButtons.forEach(button => button.addEventListener('mouseover', (e) => {
    e.target.setAttribute('style', 'background-color: #EC994B')
 })) 
 
-allButtons.forEach(element => element.addEventListener('mouseout', (e) => {
+allButtons.forEach(button => button.addEventListener('mouseout', (e) => {
    e.target.setAttribute('style', 'background-color: #FCF8EC')
 }))
 
@@ -38,51 +39,7 @@ form.addEventListener('submit', (e) => {
 });
 
 
-/*------------------EVENT HANDLERS------------------*/
-
-function createNewCard(addBrewery) {  
-   let li = document.createElement('li')
-   let h5 = document.createElement('h5')
-   let div = document.createElement('div')
-   li.textContent = addBrewery
-   h5.textContent = `State: ${state.value}`
-
-   li.appendChild(h5)
-   div.appendChild(li)
-
-   breweryContent.appendChild(div)
-
-   let newCard = {
-      name: addBrewery,
-      state: state.value
-   }
-   allBreweries.push(newCard)
-}
-
-function displayBreweryList() {
-   for (let brewery of allBreweries) {
-      const card = document.createElement('div')
-      const li = document.createElement('li')
-      li.innerText = brewery.name
-       
-      const h5 = document.createElement('h5')
-      h5.innerText = `State: ${brewery.state}`
-
-      li.appendChild(h5)
-      card.appendChild(li)
-      breweryContent.appendChild(card)
-
-      bottomContainer.style.display = 'block' 
-      breweryButton.innerText = "Hide Breweries"
-   }
-}
-
-function removeBreweryList() {
-   breweryContent.innerHTML = ""
-   bottomContainer.style.display = 'none' 
-   breweryButton.innerText = "Show Breweries"
-}
-/*------------------FUNCTIONS------------------*/
+/*------------------FUNCTIONS ------------------*/
 
 function initializeHomePage() {
    const h1 = document.createElement('h1')
@@ -109,3 +66,50 @@ function fetchandCopyData() {
          )
       })
 }
+
+
+function displayBreweryList() {
+   for (let brewery of allBreweries) {
+      const card = document.createElement('div')
+      const li = document.createElement('li')
+      li.innerText = brewery.name
+      
+      const h5 = document.createElement('h5')
+      h5.innerText = `State: ${brewery.state}`
+
+      li.appendChild(h5)
+      card.appendChild(li)
+      breweryContent.appendChild(card)
+
+      bottomContainer.style.display = 'block' 
+      breweryButton.innerText = "Hide Breweries"
+   }
+}
+
+function removeBreweryList() {
+   breweryContent.innerHTML = ""
+   bottomContainer.style.display = 'none' 
+   breweryButton.innerText = "Show Breweries"
+}
+
+function createNewCard(addBrewery) {  
+   let li = document.createElement('li')
+   let h5 = document.createElement('h5')
+   let div = document.createElement('div')
+   li.textContent = addBrewery
+   h5.textContent = `State: ${state.value}`
+
+   li.appendChild(h5)
+   div.appendChild(li)
+
+   breweryContent.appendChild(div)
+
+   let newCard = {
+      name: addBrewery,
+      state: state.value
+   }
+   allBreweries.push(newCard)
+  
+}
+
+
